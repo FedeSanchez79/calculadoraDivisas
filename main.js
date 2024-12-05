6a022e722e55b402ff06dab8097d588558102015
Fede1979 <fedesanchez@gmail.com>
2024-12-04 21:19:18 -0300

    objeto divisas

--- main.js
+++ main.js
@@ -1,117 +1,67 @@
-let acceso = prompt("Desea realizar una operación? SI/NO")
+let acceso = prompt("Desea realizar una operación de COMPRA/VENTA de divisas extranjeras con pesos Argentinos? SI/NO")
 let ingreso = "si"
 
 while (acceso == ingreso) {
+     
+    let menu1 = prompt("Por favor seleccione un número: \n" + "1.- Comprar divisas \n" + "2.- Vender divisas \n" + "3.- Salir del menu")  
+    
+    function menuDivisas(articulo, opcion, mensaje){
 
-    let menu1 = prompt("Por favor seleccione un número: \n" + "1.- Comprar divisas \n" + "2.- Vender divisas \n" + "3.- Salir del menu")
+        let menu2 = prompt("Por favor seleccione un número para indicar con que divisa desea operar: \n" + "1.- Dolares \n" + "2.- Euros \n" + "3.- Reales \n" + "4.- Pesos Chilenos \n" + "5.- Pesos Uruguayos \n" + "6.- Volver al menú anterior")
+        
+        const cotizaciones = {
+            1: { nombre: "dolares", cotizacion: 1080 },
+            2: { nombre: "euros", cotizacion: 1029 },
+            3: { nombre: "reales", cotizacion: 170 },
+            4: { nombre: "pesos Chilenos", cotizacion: 100 },
+            5: { nombre: "pesos Uruguayos", cotizacion: 23 },
+          };
+
+        switch(menu2){
+            case 1:
+                alert(`Usted selecciono operar con ${dolares}`)               
+            case 2:
+                alert(`Usted selecciono operar con ${euros}`)
+            case 3:
+                alert(`Usted selecciono operar con ${reales}`)
+            case 4:
+                alert(`Usted selecciono operar con ${pesosChi}`)
+            case 5:
+                alert(`Usted selecciono operar con ${pesosUru}`) 
+            case 6:
+                alert("Usted selecciono VOLVER")            
+            }
+        
+        let cantidad = parseFloat(prompt("Indique la cantidad que desea " + opcion + ": "))
+
+        function calculadora(monto, cotizacion, moneda){
+            alert (articulo + opcion + ` ${cantidad} ` + moneda +  mensaje + monto*cotizacion + " pesos Argentinos")
+        } 
+
+        let divisaSeleccionada = cotizaciones[menu2];
+
+            if (divisaSeleccionada) {
+  
+            calculadora(cantidad, divisaSeleccionada.cotizacion, divisaSeleccionada.nombre);
+            }else{
+                alert("Vuelva a ingresar una opción válida");
+            }
+        }
 
     if (menu1 == 1){
         alert("Usted seleccionó COMPRAR divisas")
-
-        function menuDivisasCompra(){
-
-            let menu2 = prompt("Por favor seleccione un número para indicar con que divisa desea operar: \n" + "1.- Dolares \n" + "2.- Euros \n" + "3.- Reales \n" + "4.- Pesos Chilenos \n" + "5.- Pesos Uruguayos \n")
             
-            let dolares = "Dolares"
-            let euros = "Euros"
-            let reales = "Reales"
-            let pesosChi = "Pesos Chilenos"
-            let pesosUru = "Pesos Uruguayos"
-
-            switch(menu2){
-                case 1:
-                    alert(`Usted selecciono operar con ${dolares}`)               
-                case 2:
-                    alert(`Usted selecciono operar con ${euros}`)
-                case 3:
-                    alert(`Usted selecciono operar con ${reales}`)
-                case 4:
-                    alert(`Usted selecciono operar con ${pesosChi}`)
-                case 5:
-                    alert(`Usted selecciono operar con ${pesosUru}`)   
-            }
-            
-            let cantidad = parseFloat(prompt("Indique la cantidad que desea comprar: "))
-
-            function calculadora(monto, cotizacion, moneda){
-                alert (`Para comprar ${cantidad} ` + moneda +  "usted necesita cambiar: " + monto*cotizacion + " Pesos Argentinos")
-            } 
-
-                if (menu2 == 1){
-                    calculadora(cantidad, 1080, dolares) 
-                }else if (menu2 == 2){
-                    calculadora(cantidad, 1029, euros) 
-                }else if(menu2 == 3){
-                    calculadora(cantidad, 170, reales) 
-                }else if(menu2 == 4){
-                    calculadora(cantidad, 100, pesosChi) 
-                }else if(menu2 == 5){
-                    calculadora(cantidad, 23, pesosUru) 
-                }else{
-                    alert("Vuelva a ingresar una opción válida")
-                }
-            }
-
-            menuDivisasCompra()
+            menuDivisas("Para ", "comprar", " usted necesita cambiar:")
 
     }else if (menu1 == 2) {
+
         alert("Usted seleccionó VENDER divisas")
-
-        function menuDivisasVender(){
-
-            let menu2 = prompt("Por favor seleccione un número para indicar con que divisa desea operar: \n" + "1.- Dolares \n" + "2.- Euros \n" + "3.- Reales \n" + "4.- Pesos Chilenos \n" + "5.- Pesos Uruguayos \n" + "6.- Salir")
-            
-            let dolares = "Dolares"
-            let euros = "Euros"
-            let reales = "Reales"
-            let pesosChi = "Pesos Chilenos"
-            let pesosUru = "Pesos Uruguayos"
-
-            switch(menu2){
-                case 1:
-                    alert(`Usted selecciono operar con ${dolares}`)               
-                case 2:
-                    alert(`Usted selecciono operar con ${euros}`)
-                case 3:
-                    alert(`Usted selecciono operar con ${reales}`)
-                case 4:
-                    alert(`Usted selecciono operar con ${pesosChi}`)
-                case 5:
-                    alert(`Usted selecciono operar con ${pesosUru}`)
-                case 6:
-                    alert("Usted selecciono VOLVER")    
-            }
-            
-            let cantidad = parseFloat(prompt("Indique la cantidad que desea vender: "))
-
-            function calculadora(monto, cotizacion, moneda){
-                alert (`Por vender ${cantidad}`  + moneda +  "usted recibirá: " + monto*cotizacion + " Pesos Argentinos")
-            } 
-
-                if (menu2 == 1){
-                    calculadora(cantidad, 1100, dolares) 
-                }else if (menu2 == 2){
-                    calculadora(cantidad, 1035, euros) 
-                }else if(menu2 == 3){
-                    calculadora(cantidad, 171, reales) 
-                }else if(menu2 == 4){
-                    calculadora(cantidad, 101, pesosChi) 
-                }else if(menu2 == 5){
-                    calculadora(cantidad, 24, pesosUru) 
-                }else if(menu == 6){
-                    alert("Regresamos al menu anterior")
-                }
-                else{
-                    alert("Opción no válida")
-                }
-            }
-
-            menuDivisasVender()
         
-    }else {
-        alert("Gracias por utilizar nuestros servicios")
-        break      
-    }
-}
+            menuDivisas("Al ", "vender", " usted recibirá:")
 
+    }else {
+            alert("Gracias por utilizar nuestros servicios")  
+            break    
+        }    
+    }
 alert("Programa finalizado")
